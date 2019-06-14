8f9f663e9cb73a6b2e4785a56872f3d3a1a92d32
Yifan Xu <yifanxu00@outlook.com>
2019-06-14 12:31:57 -0700

    Instead of fixing just make a new proj

---
+++ Projects/MainBuild_II/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject focus;
+    public float LerpFactor = 0.7f;
+
+    private float zConstant;
+    private Transform transf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transf = this.GetComponent<Transform>();
+        zConstant = transf.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos = Vector3.Lerp(pos, focus.GetComponent<Transform>().position, LerpFactor);
+        pos.z = zConstant;
+        transf.position = pos;
+    }
+}
---
+++ Projects/MainBuild_II/Assets/Scripts/ShrimpBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class ShrimpBehavior : MonoBehaviour
+    {
+        public float speed;
+        public float rotateAngle;
+        public float shootAngle;
+        public GameObject shrimpPrefab;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 rotation = this.GetComponent<Transform>().eulerAngles;
+            rotation.z += rotateAngle;
+            this.GetComponent<Transform>().eulerAngles = rotation;
+
+            Vector3 position = this.GetComponent<Transform>().position;
+            position.x += speed * Mathf.Cos(shootAngle);
+            position.y += speed * Mathf.Sin(shootAngle);
+            this.GetComponent<Transform>().position = position;
+        }
+    }
+}
