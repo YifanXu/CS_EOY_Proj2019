20fc1e72424828a74eb4bff2cefa5cb65a5ccbae
YifanXu <yifanxu00@outlook.com>
2019-06-17 21:05:37 -0700

    actually add the files for previous changes

---
+++ Projects/MainBuild_II/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ButtonScript : MonoBehaviour
+    {
+        public Sprite pressedSprite;
+        public Sprite unPressedSprite;
+        public Output[] outputs;
+        public LayerMask entityLayer;
+        public bool isPressed = false;
+        public float delay = 1f;
+
+        private Bounds bounds;
+        private SpriteRenderer spriteRen;
+        public float timer = 0f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            timer = -1f;
+            bounds = this.GetComponent<Collider2D>().bounds;
+            spriteRen = GetComponent<SpriteRenderer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var center = bounds.center;
+            var size = bounds.size;
+            //Collide?
+            bool isOverlapping = Physics2D.OverlapArea(
+                new Vector2(center.x - size.x / 2f, center.y - size.y / 2f),
+                new Vector2(center.x + size.x / 2f, center.y + size.y / 2f + 0.01f),
+                entityLayer
+                );
+            if (!isOverlapping && isPressed)
+            {
+                //Unpress plz
+                isPressed = false;
+                //Set Timer
+                timer = delay;
+            }
+            else if(isOverlapping && !isPressed)
+            {
+                isPressed = true;
+                if (timer < 0f)
+                {
+                    //Press
+                    TriggerAll();
+                    spriteRen.sprite = pressedSprite;
+                }
+            }
+
+            if (timer >= 0f)
+            {
+                timer -= Time.deltaTime;
+                if(timer < 0f && !isPressed)
+                {
+                    //Actually unpress
+                    TriggerAll();
+                    spriteRen.sprite = unPressedSprite;
+                }
+            }
+        }
+
+        private void TriggerAll()
+        {
+            Debug.Log("TRIGGER!");
+            foreach(Output o in outputs)
+            {
+                o.Trigger();
+            }
+        }
+    }
+}
---
+++ Projects/MainBuild_II/Assets/Scripts/DashAbility.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class DashAbility : Ability
+    {
+        public float strength = 10f;
+        private TrailRenderer trail;
+        private PlayerScript movement;
+
+        public float timer;
+        private float trailLinger = 0.2f;
+        private float dashTime = 0.1f;
+        public float tempMomentumDelta = 0f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            trail = gameObject.GetComponent<TrailRenderer>();
+            trail.enabled = false;
+            movement = gameObject.GetComponent<PlayerScript>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(timer > 0f)
+            {
+                if(tempMomentumDelta != 0f && timer < trailLinger - dashTime)
+                {
+                    movement.momentum -= tempMomentumDelta;
+                    tempMomentumDelta = 0f;
+                }
+                timer -= Time.deltaTime;
+                //trail.time = timer;
+            }
+            else
+            {
+                trail.enabled = false;
+            }
+        }
+
+        public override void Activate()
+        {
+            if (timer <= trailLinger - dashTime)
+            {
+                trail.enabled = true;
+                trail.time = trailLinger;
+                timer = trailLinger;
+                tempMomentumDelta = strength * (movement.momentum >= 0f ? 1 : -1);
+                movement.momentum += tempMomentumDelta;
+            }
+        }
+    }
+}
