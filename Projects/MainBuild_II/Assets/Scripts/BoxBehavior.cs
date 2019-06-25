46001071b9a5d14c8c1db0d6b73e32e899d94db4
MechaDragonX <sonicraghav@gmail.com>
2019-06-24 17:53:31 -0700

    Finish Bloby Boxy enemy

--- Projects/MainBuild_II/Assets/Scripts/BoxBehavior.cs
+++ Projects/MainBuild_II/Assets/Scripts/BoxBehavior.cs
@@ -2,35 +2,120 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoxBehavior : MonoBehaviour
+namespace Assets.Scripts
 {
-    public float speed;
-    public float rayDistance;
-    private bool movingRight = true;
-    public Transform groundDetection;
-
-    // Start is called before the first frame update
-    void Start()
+    public class BoxBehavior : MonoBehaviour
     {
-        
-    }
+        public float speed;
+        public float rayDistance;
+        private Direction direction = Direction.right;
+        public Transform groundDetection;
+        // private RayCollider2D rayCollider;
+        private BoxCollider2D collider;
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, rayDistance);
-        if (!groundInfo.collider)
+        // Start is called before the first frame update
+        void Start()
         {
-            if (movingRight)
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //transform.Translate(Vector2.right * speed * Time.deltaTime);
+            //rayCollider = new RayCollider2D(groundDetection.position, rayDistance);
+            //if (!rayCollider.groundDown.collider && rayCollider.groundLeft.collider)
+            //{
+            //    transform.eulerAngles = new Vector3(0.0f, 0.0f, -270.0f);
+            //}
+            //else if
+            //    (
+            //        (!rayCollider.groundLeft.collider && rayCollider.groundUp.collider) ||
+            //        (!rayCollider.groundUp.collider && rayCollider.groundRight.collider) ||
+            //        (!rayCollider.groundRight.collider && rayCollider.groundDown.collider)
+            //    )
+            //{
+            //    transform.eulerAngles = new Vector3(0.0f, 0.0f, -90.0f);
+            //}
+
+            collider = this.GetComponent<BoxCollider2D>();
+            if ( // Bottom is touching
+                    Physics2D.OverlapArea
+                    (
+                        new Vector2
+                        (
+                            collider.bounds.center.x - (collider.size.x / 2),
+                            collider.bounds.center.y - (collider.size.y / 2)
+                        ),
+                        new Vector2
+                        (
+                            collider.bounds.center.x + (collider.size.x / 2),
+                            collider.bounds.center.y - (collider.size.y / 2)
+                        )
+                    )
+               )
             {
-                transform.eulerAngles = new Vector3(0.0f, -180.0f, 0.0f);
-                movingRight = false;
+                transform.eulerAngles = new Vector3(0.0f, 0.0f, -270.0f);
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
             }
-            else
+            else if
+                ( // Top is touching
+                    Physics2D.OverlapArea
+                    (
+                        new Vector2
+                        (
+                            collider.bounds.center.x - (collider.size.x / 2),
+                            collider.bounds.center.y + (collider.size.y / 2)
+                        ),
+                        new Vector2
+                        (
+                            collider.bounds.center.x + (collider.size.x / 2),
+                            collider.bounds.center.y + (collider.size.y / 2)
+                        )
+                    )
+               )
             {
-                transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
-                movingRight = true;
+                transform.eulerAngles = new Vector3(0.0f, 0.0f, -270.0f);
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
+            }
+            else if
+                ( // Left is touching
+                    Physics2D.OverlapArea
+                    (
+                        new Vector2
+                        (
+                            collider.bounds.center.x - (collider.size.x / 2),
+                            collider.bounds.center.y + (collider.size.y / 2)
+                        ),
+                        new Vector2
+                        (
+                            collider.bounds.center.x - (collider.size.x / 2),
+                            collider.bounds.center.y - (collider.size.y / 2)
+                        )
+                    )
+               )
+            {
+                transform.eulerAngles = new Vector3(0.0f, 0.0f, -270.0f);
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
+            }
+            else if
+                ( // Right is touching
+                    Physics2D.OverlapArea
+                    (
+                        new Vector2
+                        (
+                            collider.bounds.center.x + (collider.size.x / 2),
+                            collider.bounds.center.y + (collider.size.y / 2)
+                        ),
+                        new Vector2
+                        (
+                            collider.bounds.center.x + (collider.size.x / 2),
+                            collider.bounds.center.y - (collider.size.y / 2)
+                        )
+                    )
+               )
+            {
+                transform.eulerAngles = new Vector3(0.0f, 0.0f, -270.0f);
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
             }
         }
     }
